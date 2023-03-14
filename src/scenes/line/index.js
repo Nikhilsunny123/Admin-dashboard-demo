2b0d9a436ca747e644b96cd792da898c8ed5423d
Nikhilsunny123 <nikhilsunny35@gmail.com>
2023-03-14 22:33:13 +0530

    added charts

---
+++ src/scenes/line/index.js
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+import LineChart from "../../components/LineChart";
+const Bar = () => {
+  return (
+    <Box m="20px">
+      <Header title="LINE CHART" subtitle="View Line Chart" />
+      <Box height="75vh">
+        <LineChart />
+      </Box>
+    </Box>
+  );
+};
+
+export default Bar;
