aba0811520ca17059e671cd051c7615bae99d8af
Nikhilsunny123 <nikhilsunny35@gmail.com>
2023-03-15 11:19:10 +0530

    done

---
+++ src/components/StatBox.js
@@ -0,0 +1,43 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { tokens } from "../theme";
+import ProgressCircles from "./ProgressCircles";
+const StatBox = ({ title, subtitle, icon, progress, increase }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box width="100%" m="0 30px">
+      <Box display="flex" justifyContent="space-between">
+        <Box>
+          {icon}
+          <Typography
+            variant="h4"
+            fontWeight="bold"
+            sx={{ color: colors.grey[100] }}
+          >
+            {title}
+          </Typography>
+        </Box>
+        <Box>
+          <ProgressCircles progress={progress} />
+        </Box>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
+            {subtitle}
+          </Typography>
+        </Box>
+        <Box>
+          {icon}
+          <Typography
+            variant="h5"
+            fontStyle="italic"
+            sx={{ color: colors.greenAccent[600] }}
+          >
+            {increase}
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default StatBox;
